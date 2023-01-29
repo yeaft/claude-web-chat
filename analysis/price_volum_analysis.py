6a249f321c2c8c698c50247573f838bb8f5c9096
YiHe <122829105@qq.com>
2023-01-29 02:56:28 +0000

    .

--- analysis/price_volum_analysis.py
+++ analysis/price_volum_analysis.py
@@ -1,5 +1,6 @@
 import click
 from helper import constance, utils, date_utils
+from statistics import mean, variance, stdev
 
 def update_sum_ccl(contract_type):
     main_col = constance.FUTURE_DB['tick_{}_main'.format(contract_type)]
@@ -35,7 +36,7 @@ def daily_min_max_ccl(contract_type, start_date, end_date):
     results = []
     try:
         while current_date < end_date:
-            start_time = "{} 09:00:00.000".format(current_date)
+            start_time = "{} 21:00:00.000".format(current_date)
             current_date = date_utils.datestr_add_days(current_date, 1, "-")
             end_time = "{} 15:00:00.000".format(current_date)
             min_ccls = list(main_col.find({'time':{"$lte": end_time, "$gte": start_time}}).sort("ccl",1).limit(1))
@@ -60,8 +61,9 @@ def daily_min_max_ccl(contract_type, start_date, end_date):
             results.append(result)
             utils.log("Finish {} - {}".format(start_time, end_time))
         
-        utils.convert_dics_str("ccl_min_max", results)        
+        utils.convert_dic_to_csv("ccl_min_max", results)        
     except Exception as e:
+        utils.log(e)
         utils.log("Results: {}".format(results))
         
     return
@@ -69,23 +71,61 @@ def daily_min_max_ccl(contract_type, start_date, end_date):
 def ccl_abnormal():
     return
 
-# Once pass this check send a alert, to prepare the trade
-# 0. if far away from price detected, then change back
-# 1. continous two units >= mean + 2 * stdev
-# 2. Or one unit mean + 4 * stdev
-# 3. Or past 6 unit sum > past 3 mins sum
+
+# 1. current past 10s larger than past 10-40s summary.
+# 2. current 10s is 3 times than past 10-20s
+# 3. current 10s is larger than a threshold （Average in the last day?111）
 def cjl_abnormal(contract_type, start_date, end_date):
     main_col = constance.FUTURE_DB['tick_{}_main'.format(contract_type)]
     # max_ main_col.find({'time':{"$lte": end_date, "$gte": start_date}}).sort("time", 1):
     return 
 
 # Refer to past 5 days, day or night cjl avg and stdev.
-def cjl_avg_dev(contract_type, start_time, end_time):
+def cjl_avg_dev(contract_type, start_date):
+    main_col = constance.FUTURE_DB['tick_{}_main'.format(contract_type)]
+    cjl_daily_statics = []
+    while start_date <= "2022-12-11":
+        for i in range(0, 2):
+            cjls = []
+            if i == 0:
+                start_time = "{} 21:00:00.000".format(start_date)
+                start_date = date_utils.datestr_add_days(start_date, 1, "-")
+                end_time = "{} 13:30:00.000".format(start_date)
+            else:
+                start_time = "{} 09:00:00.000".format(start_date)
+                end_time = "{} 15:00:00.000".format(start_date)
 
+            for t in main_col.find({"time": time_filter(start_time, end_time)}):
+                cjls.append(t['cjl'])
+            if len(cjls) > 0:
+                cjl_daily_statics.append({
+                    "date": start_date,
+                    "isDay": i,
+                    "cjlAvg": mean(cjls),
+                    "cjlStdev": stdev(cjls)
+                })
+                utils.log("{}, {}, {}, {}".format(start_date, i, mean(cjls), len(cjls)))
+    
+    utils.log(cjl_daily_statics)
+    constance.FUTURE_DB['tick_{}_cjl_daily_statics'.format(contract_type)].insert_many(cjl_daily_statics)
         
         
 
+    # day_ticks = get_yesterday_day_ticks_by_date("sa209", "20220408", "090000", "150000")
+    # night_ticks = get_yesterday_day_ticks_by_date("sa209", "20220408", "210000", "230000")
+    # update_factors(day_ticks, {})
+    # update_factors(night_ticks, {})
+    return
+
+def time_filter(start_time, end_time):
+    return {
+        "$gte": start_time,
+        "$lte": end_time
+    }
+    
 if __name__ == "__main__":
     # update_sum_ccl("rb")
     # output_csv("rb")
-    daily_min_max_ccl("rb", "2021-03-03", "2021-04-03")
+    # cjl_avg_dev("rb", "2020-01-01")
+    daily_min_max_ccl("rb", "2021-12-01", "2021-04-03")
+
