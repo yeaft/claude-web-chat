b8bb9cb112afd6059fd303e6748178fd79eb96a4
hyi <122829105@qq.com>
2023-10-26 19:39:56 +0800

    .

---
+++ api_remove_and_deploy copy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo docker stop realtime-server && sudo docker rm realtime-server && sudo docker image rm realtime-server
+docker build -t realtime-server -f ServerDockerFile --no-cache . && sudo docker run --name realtime-server --network host --restart=unless-stopped -d realtime-server -p 80:80
