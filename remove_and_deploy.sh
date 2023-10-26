5ef525e8f6f2a6d802a3125d445c2205aa8d7d0c
hyi <122829105@qq.com>
2023-10-26 19:03:32 +0800

    .

--- remove_and_deploy.sh
+++ remove_and_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker stop realtime-collector && sudo docker rm realtime-collector && sudo docker image rm
+sudo docker stop realtime-collector && sudo docker rm realtime-collector && sudo docker image rm realtime-collector
 sudo docker build -t realtime-collector --no-cache . && sudo docker run --name realtime-collector --network host --restart=unless-stopped -d realtime-collector
