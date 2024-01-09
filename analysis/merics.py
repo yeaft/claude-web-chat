e55d765648fc5e84f3d03b3c32962c415b026409
tom <122829105@qq.com>
2024-01-09 22:01:04 +0800

    .

--- analysis/merics.py
+++ analysis/merics.py
@@ -5,7 +5,7 @@ class Metric:
     __ccl_trend_values = ["DownEnd", "Down", "UpAdjust", "Adjust", "DownAdjust", "Up", "UpEnd"]
     __price_trend_values = ["DownEnd", "Down", "UpAdjust", "Adjust", "DownAjust", "Up", "UpEnd"]
     __price_micro_trend_values = ["Down", "Adjust", "Up"]
-    __cjl_status_values = ["Lowest", "Low", "Normal", "High", "Highest"]
+    __cjl_status_values = ["Cold", "Warm", "Hot", "StartHot", "Cooling"]
     __contract_status_values = ["Long", "LongPullback", "Adjust", "ShortPullback", "Short"]
     __contract_trend_values = ["Long", "LongPullback", "Balance", "ShortPullback", "Short"]
     __close_ccl_trend_values = ["Down", "Adjust", "Up"]
@@ -24,7 +24,9 @@ class Metric:
     #     self.ccl_hot_threshold = int(9000000/reference_price)
     
     __low_cjl_minute_threshold = 2000
+    __hot_cjl_minute_threshold = 6000
     __five_minute_check_point_count = 5 * 12
+    __six_hours_check_point_count = 6 * 60 * 12
     __balance_avg_to_min_max_price_diff_rate = 0.0008
     __balance_avg_to_min_max_ccl_diff = 100
 
@@ -92,28 +94,61 @@ class Metric:
     def calculate_contract_status(self, tick_infos, day_infos):
         pass
 
-    def calculate_contract_trend(self, tick_infos, day_infos):
-
+    def calculate_contract_trend(self, tick_infos, day_infos):        
         pass
+    
+    def get_current_metrics(self, tick_infos, day_infos):        
+        metric = {}
+        if len(tick_infos) < self.__six_hours_check_point_count:
+            return 
 
-    def is_balance(self, tick_infos, day_infos):
-        if len(tick_infos) < self.__five_minute_check_point_count:
-            return False
+        extrems = self.find_extremes(tick_infos[-self.__six_hours_check_point_count:])
+        # 确定大方向
+        contract_status = self.get_contract_status(tick_infos, extrems)
+        metric['contractStatus'] = contract_status
         
-        sub_ticks = tick_infos[-self.__five_minute_check_point_count:]
-        cjls = [tick['cjlDiff'] for tick in sub_ticks]
+        past_five_mins_ticks = tick_infos[-self.__five_minute_check_point_count:]
+        cjls = [tick['cjlDiff'] for tick in past_five_mins_ticks]
+        metric['cjlStatus'] = self.get_cjl_status(cjls)
+
+        prices = [tick['zxj'] for tick in past_five_mins_ticks]
+        ccls = [tick['ccl'] for tick in past_five_mins_ticks]
+        if metric['cjlStatus'] == "Cold" and self.is_balance(ccls, prices, cjls):
+            metric['contractTrend'] = "Balance"
+    
+    def get_zxj_trend(self, prices):
+        
+        
+    def get_cjl_status(self, cjls):
+        #    __cjl_status_values = ["Cold", "Warm", "Hot", "StartHot", "Cooling"]
+        minutes_cjls = []
+        for i in range(0, 5):
+            minutes_cjls.append(sum(cjls[i * 12 : (i + 1) * 12]))
+        
+        if minutes_cjls[-1] >= self.__hot_cjl_minute_threshold:
+            return "Hot"
+        elif minutes_cjls[-1] >= self.__low_cjl_minute_threshold:
+            if minutes_cjls[-1] > 1.3 * minutes_cjls[-2]:
+                return "StartHot"
+            elif minutes_cjls[-1] < 0.7 * minutes_cjls[-2]:
+                return "Cooling"
+            else:
+                return "Warm"
+        else:
+            return "Cold"
+
+    def is_balance(self, ccls, prices, cjls):
+
         for i in range(0, 5):
             if sum(cjls[i * 12 : (i + 1) * 12]) > self.__low_cjl_minute_threshold:
-                return False
+                return False        
         
-        prices = [tick['zxj'] for tick in sub_ticks]
         avg_price = sum(prices) / len(prices)
         min_price = min(prices)
         max_price = max(prices)
         if (max_price - avg_price) / avg_price > self.__balance_avg_to_min_max_price_diff_rate or (avg_price - min_price) / avg_price > self.__balance_avg_to_min_max_price_diff_rate:
             return False   
-
-        ccls = [tick['ccl'] for tick in sub_ticks]
+        
         avg_ccl = sum(ccls) / len(ccls)
         min_ccl = min(ccls)
         max_ccl = max(ccls)
@@ -121,6 +156,72 @@ class Metric:
             return False
         
         return True
+
+    def get_contract_status(self, tick_infos, extrems):
+        if len(extrems) < 2:
+            return None
+
+        long_count, short_count, long_pullback_count, short_pullback_count = 0, 0, 0, 0
+        for i in range(len(extrems) - 2):
+            price_diff = extrems[i+1]['zxj'] - extrems[i]['zxj']
+            ccl_diff = extrems[i+1]['ccl'] - extrems[i]['ccl']
+            if price_diff > 0 and ccl_diff > 0:
+                long_count += 1
+            elif price_diff < 0 and ccl_diff < 0:
+                long_pullback_count += 1
+            elif price_diff > 0 and ccl_diff < 0:
+                short_pullback_count += 1
+            elif price_diff < 0 and ccl_diff > 0:
+                short_count += 1
+        
+        all_count = long_count + short_count
+        all_pullback_count = long_pullback_count + short_pullback_count
+        count_info = f"{long_count},{short_count},{long_pullback_count},{short_pullback_count}"
+        if all_count == 0:
+            if long_pullback_count / all_pullback_count > 0.7:
+                status = "LongPullback"
+            elif short_pullback_count / all_pullback_count > 0.7:
+                status = "ShortPullback"
+            status = "Unknown"        
+        elif long_count / all_count > 0.7:
+            status = "Long"
+        elif short_count / all_count > 0.7:
+            status = "Short"
+        else:
+            if long_pullback_count / all_pullback_count > 0.7:
+                status = "LongPullback"
+            elif short_pullback_count / all_pullback_count > 0.7:
+                status = "ShortPullback"
+            status = "Unknown"
+        
+        return f"{status},{count_info}"
+            
+    def find_extremes(self, ticks, span = 45 * 12):        
+        extremes = []  # 用于存储最高和最低值的索
+        for i in range(len(ticks)):
+            # 确定窗口边界
+            start = max(0, i - span)
+            end = min(len(ticks), i + span + 1)
+
+            # 当前值
+            current_value = ticks[i]['zxj']
+            min_value = min(t['zxj'] for t in ticks[start:end])
+            max_value = max(t['zxj'] for t in ticks[start:end])
+            # 检查是否为最高或最低值
+            if current_value == max_value:
+                ticks[i]['extreme'] = 'max'
+                if len(extremes) > 0 and extremes[-1]['extreme'] == 'max' and current_value >= extremes[-1]['zxj']:
+                    extremes[-1] = ticks[i]
+                else:
+                    extremes.append(ticks[i])
+            elif current_value == min_value:
+                ticks[i]['extreme'] = 'min'
+                if len(extremes) > 0 and extremes[-1]['extreme'] == 'min' and current_value <= extremes[-1]['zxj']:
+                    extremes[-1] = ticks[i]
+                else:
+                    extremes.append(ticks[i])
+
+        return extremes
 
     def calculate_close_ccl_trend(self, tick_infos, day_infos):
         pass
@@ -131,18 +232,18 @@ class Metric:
     def calculate_close_price_micro_trend(self, tick_infos, day_infos):
         pass
 
-    def get_current_metrics(self, tick_infos, day_infos):
-        ccl_status =  self.calculate_ccl_status(tick_infos, day_infos)
-        ccl_space =  self.calculate_ccl_space(tick_infos, day_infos)
-        ccl_trend =  self.calculate_ccl_trend(tick_infos, day_infos)
-        price_status = self.calculate_price_status(tick_infos, day_infos)
-        price_trend = self.calculate_price_trend(tick_infos, day_infos)
-        cjl_status = self.calculate_cjl_status(tick_infos, day_infos)
-        contract_status = self.calculate_contract_status(tick_infos, day_infos)
-        contract_trend = self.calculate_contract_trend(tick_infos, day_infos)
-        close_ccl_trend = self.calculate_close_ccl_trend(tick_infos, day_infos)
-        close_price_trend = self.calculate_close_price_trend(tick_infos, day_infos)
-        close_price_micro_trend = self.calculate_close_price_micro_trend(tick_infos, day_infos)
+    # def get_current_metrics(self, tick_infos, day_infos):
+    #     ccl_status =  self.calculate_ccl_status(tick_infos, day_infos)
+    #     ccl_space =  self.calculate_ccl_space(tick_infos, day_infos)
+    #     ccl_trend =  self.calculate_ccl_trend(tick_infos, day_infos)
+    #     price_status = self.calculate_price_status(tick_infos, day_infos)
+    #     price_trend = self.calculate_price_trend(tick_infos, day_infos)
+    #     cjl_status = self.calculate_cjl_status(tick_infos, day_infos)
+    #     contract_status = self.calculate_contract_status(tick_infos, day_infos)
+    #     contract_trend = self.calculate_contract_trend(tick_infos, day_infos)
+    #     close_ccl_trend = self.calculate_close_ccl_trend(tick_infos, day_infos)
+    #     close_price_trend = self.calculate_close_price_trend(tick_infos, day_infos)
+    #     close_price_micro_trend = self.calculate_close_price_micro_trend(tick_infos, day_infos)
 
 
     
