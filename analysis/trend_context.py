71781fb0f2726cff8c97802d560b8c617a6a64c2
tom <122829105@qq.com>
2024-11-06 16:06:56 +0800

    remove useless code

--- analysis/trend_context.py
+++
@@ -1,9 +0,0 @@
-class trend_context:
-    def __init__(self, trend_min_diff_rate = 0.05):
-        self.context = {
-            "status": "unknown",
-            "last_peak_price": 0,
-            "last_peak_time": "",
-            "last_peak_type": "unknown",
-            "trend_min_diff_rate": trend_min_diff_rate
-        }
