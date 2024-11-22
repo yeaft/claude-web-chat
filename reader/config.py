98c15d3bc6881805d04853a7df4bb37f543acb43
tom <122829105@qq.com>
2024-11-22 13:58:28 +0800

    .

--- reader/config.py
+++ reader/config.py
@@ -2,4 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    MONGO_URI = 'mongodb://localhost:27017/dao'
+    MONGODB_HOST = os.getenv("MONGODB_HOST", "localhost")
+    MONGO_URI = f'mongodb://{MONGODB_HOST}:27017/dao'
