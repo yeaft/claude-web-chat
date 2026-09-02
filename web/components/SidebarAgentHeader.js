11beec6ed9c433be0b3bc6c73772fa7d24f47e4a
YiHe <122829105@qq.com>
2026-09-02 08:05:53 +0000

    fix: tune agent list typography

--- web/components/SidebarAgentHeader.js
+++ web/components/SidebarAgentHeader.js
@@ -71,7 +71,7 @@ export default {
                 :aria-label="tr('chat.agent.upgrade', 'Upgrade')"
               >
                 <span v-if="upgradingAgents[agent.id]" class="spinner-mini"></span>
-                <svg v-else viewBox="0 0 24 24" width="16" height="16" aria-hidden="true"><path fill="currentColor" d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"/></svg>
+                <svg v-else viewBox="0 0 24 24" width="14" height="14" aria-hidden="true"><path fill="currentColor" d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"/></svg>
               </button>
               <button
                 type="button"
@@ -82,7 +82,7 @@ export default {
                 :aria-label="tr('chat.agent.restart', 'Restart')"
               >
                 <span v-if="restartingAgents[agent.id]" class="spinner-mini"></span>
-                <svg v-else viewBox="0 0 24 24" width="16" height="16" aria-hidden="true"><path fill="currentColor" d="M17.65 6.35A7.958 7.958 0 0012 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08A5.99 5.99 0 0112 18c-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/></svg>
+                <svg v-else viewBox="0 0 24 24" width="14" height="14" aria-hidden="true"><path fill="currentColor" d="M17.65 6.35A7.958 7.958 0 0012 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08A5.99 5.99 0 0112 18c-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"/></svg>
               </button>
             </span>
           </div>
